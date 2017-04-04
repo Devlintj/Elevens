7292d9b110fa37fce6e73e103d63fe45652b3237
Tyler James Devlin <devlintj@dcsdk12.org>
2017-04-04 12:28:25 -0600

    Exercise 2

--- src/Buggy5/Deck.java
+++ src/Buggy5/Deck.java
@@ -33,7 +33,7 @@ public class Deck {
 	 */
 	public Deck(String[] ranks, String[] suits, int[] values) {
 		cards = new ArrayList<Card>();
-		for (int j = 1; j < ranks.length; j++) {
+		for (int j = 0; j < ranks.length; j++) {
 			for (String suitString : suits) {
 				cards.add(new Card(ranks[j], suitString, values[j]));
 			}
@@ -47,7 +47,7 @@ public class Deck {
 	 * @return true if this deck is empty, false otherwise.
 	 */
 	public boolean isEmpty() {
-		return size < 0;
+		return size <= 0;
 	}
 
 	/**
@@ -63,8 +63,8 @@ public class Deck {
 	 * and reset the size to represent the entire deck.
 	 */
 	public void shuffle() {
-		for (int k = cards.size() - 1; k < 0; k--) {
-			int howMany = k + 1;
+		for (int k = cards.size() - 1; k > 0; k--) {
+			int howMany = k;
 			int start = 0;
 			int randPos = (int) (Math.random() * howMany) + start;
 			Card temp = cards.get(k);
